ad1f810c583f17142534504699c08a217ee29390
Josh Loya <jloya3@oreillyauto.com>
2025-07-31 21:30:18 -0500

    initial commit

---
+++ src/components/NewerBadge/NewerBadge.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  text: { type: String, default: "Content goes here" },
+  fontSize: { type: Number, default: 14 }
+});
+</script>
+
+<template>
+  <div :class="$style.container">
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: inline-block;
+  font-size: v-bind(fontSize + "px");
+  height: 30px;
+  color: red;
+  background-color: pink;
+  padding: 0 8px;
+  border-radius: 4px;
+}
+</style>
